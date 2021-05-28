5fd5051764285be1a6f629da08d01b2039f32d6c
Stanicc <stanicmucierres@gmail.com>
2021-05-28 15:46:10 -0300

    Fixed wrong requester in playing info

--- src/main/kotlin/stanic/miris/discord/commands/information/InfoCommand.kt
+++ src/main/kotlin/stanic/miris/discord/commands/information/InfoCommand.kt
@@ -32,7 +32,7 @@ private suspend fun CommandExecutor.runInfoCommand() {
     val embed = EmbedBuilder()
         .setTitle("Playing info")
         .setColor(LIGHT_PINK_COLOR)
-        .setDescription("<:menu:781976446418812958> Here's the playing info \n\n\uD83C\uDFA7 **Playing now**: ${playingTrack.info.title} \n⏳ **Duration:** ${getTime(playingTrack.duration)} \n     - **Time left:** ${getTime(System.currentTimeMillis() - trackModel.startedTime)} \n<:youtube:807338481990500462> **Channel:** [${playingTrack.info.author}](${playingTrack.info.uri}) \n\uD83D\uDC4C **Requested by:** ${member.nickname ?: member.user.name} \n\n\uD83D\uDD09 Volume: ${getMusicManager().getGuildPlayer(guild).volume} \n\uD83D\uDCE2 Bass mode: ${musicManager.getGuildTrackScheduler(guild).bassMode.getName()} \n${if (trackModel.loop) "\n__This song is in a loop__" else ""}")
+        .setDescription("<:menu:781976446418812958> Here's the playing info \n\n\uD83C\uDFA7 **Playing now**: ${playingTrack.info.title} \n⏳ **Duration:** ${getTime(playingTrack.duration)} \n     - **Time left:** ${getTime(System.currentTimeMillis() - trackModel.startedTime)} \n<:youtube:807338481990500462> **Channel:** [${playingTrack.info.author}](${playingTrack.info.uri}) \n\uD83D\uDC4C **Requested by:** ${trackModel.member.nickname ?: trackModel.member.user.name} \n\n\uD83D\uDD09 Volume: ${getMusicManager().getGuildPlayer(guild).volume} \n\uD83D\uDCE2 Bass mode: ${musicManager.getGuildTrackScheduler(guild).bassMode.getName()} \n${if (trackModel.loop) "\n__This song is in a loop__" else ""}")
         .setThumbnail("https://i3.ytimg.com/vi/${playingTrack.info.identifier}/maxresdefault.jpg")
         .build()
     channel.reply(embed) {
