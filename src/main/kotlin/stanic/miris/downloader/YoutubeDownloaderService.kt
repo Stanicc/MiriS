3b8e2e174811b137dfc9512f506432758cde30aa
Stanicc <stanicmucierres@gmail.com>
2021-05-31 13:23:11 -0300

    Fixed file not changing the extension in callback

--- src/main/kotlin/stanic/miris/downloader/YoutubeDownloaderService.kt
+++ src/main/kotlin/stanic/miris/downloader/YoutubeDownloaderService.kt
@@ -3,7 +3,6 @@ package stanic.miris.downloader
 import com.github.kiulian.downloader.OnYoutubeDownloadListener
 import com.github.kiulian.downloader.YoutubeDownloader
 import com.github.kiulian.downloader.YoutubeException
-import com.github.kiulian.downloader.model.quality.VideoQuality
 import com.github.kiulian.downloader.parser.DefaultParser
 import stanic.miris.downloader.callback.DownloaderCallback
 import java.io.File
@@ -27,8 +26,7 @@ class YoutubeDownloaderService {
                 }
 
                 override fun onFinished(file: File) {
-                    if (audio) file.renameTo(File(System.getProperty("user.dir") + "/temp-download" + video.details().title() + ".mp3"))
-                    callback.onFinish(file)
+                    callback.onFinish(if (audio) changeExtension(file, "mp3") else file)
                 }
 
                 override fun onError(throwable: Throwable) {
@@ -36,6 +34,15 @@ class YoutubeDownloaderService {
                 }
             })
         } catch (exception: Exception) { callback.onError(YoutubeException.BadPageException("Exception")) }
+    }
+
+    fun changeExtension(file: File, newExtension: String): File {
+        val fileExtension = file.name.lastIndexOf('.')
+        val fileName = file.name.subSequence(0, fileExtension)
+
+        return File(file.parent, "${fileName}.$newExtension").apply {
+            file.delete()
+        }
     }
 
 }
