569c8c277c5917619b1a81ed51ba81b6ff3113cc
Stanicc <stanicmucierres@gmail.com>
2021-07-12 19:42:34 -0300

    💻 Starting the website

---
+++ website/pages/index.tsx
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>MiriS</title>
+        <meta name="description" content="MiriS website" />
+      </Head>
+
+      <main>
+        <h1>
+          Hello World!
+        </h1>
+      </main>
+    </div>
+  )
+}
